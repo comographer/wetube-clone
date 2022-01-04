f1a33d039c893b11f93d65db059e1caa7680737a
Geunmo Kim <kimgeunmo@gmail.com>
2022-01-04 17:01:23 +0100

    GET request: .send()

--- src/server.js
+++ src/server.js
@@ -4,7 +4,18 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send("<h1>Message</h1>");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("<h1>Login Here</h1>");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () =>
-  console.log(`Server listening on port ${PORT} 🚀`);
+  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
