e013fb384ea13ae961b3f9f58073a5bfdf14216f
Geunmo Kim <kimgeunmo@gmail.com>
2022-01-04 23:10:41 +0100

    Add middleware

--- src/server.js
+++ src/server.js
@@ -4,18 +4,18 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("<h1>Message</h1>");
+  return res.send("I love middlewares");
 };
 
-const handleLogin = (req, res) => {
-  return res.send("<h1>Login Here</h1>");
-};
-
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", logger, handleHome);
 
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
