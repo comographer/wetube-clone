672c0972e020df488dfbc34c0be6cad424b23516
Geunmo Kim <kimgeunmo@gmail.com>
2022-01-05 23:41:01 +0100

    Routers and Controllers practice

---
+++ src/controllers/videoController.js
@@ -0,0 +1,11 @@
+export const trending = (req, res) => res.send("Trending Videos");
+
+export const see = (req, res) => res.send("Watch Video");
+
+export const edit = (req, res) => res.send("Edit Video");
+
+export const search = (req, res) => res.send("Search Video");
+
+export const upload = (req, res) => res.send("Upload Video");
+
+export const deleteVideo = (req, res) => res.send("Delete Video");
