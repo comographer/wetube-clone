0322b4166e137dad047959f7fb186b4609bb0d13
Geunmo Kim <kimgeunmo@gmail.com>
2022-01-06 12:46:52 +0100

    Add URL Params Regular Expression

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,17 @@
 export const trending = (req, res) => res.send("Trending Videos");
 
-export const see = (req, res) => res.send("Watch Video");
+export const see = (req, res) => {
+  return res.send(`Watch Video # ${req.params.id}`);
+};
 
-export const edit = (req, res) => res.send("Edit Video");
+export const edit = (req, res) => {
+  return res.send("Edit Video");
+};
 
 export const search = (req, res) => res.send("Search Video");
 
 export const upload = (req, res) => res.send("Upload Video");
 
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const deleteVideo = (req, res) => {
+  return res.send("Delete Video");
+};
