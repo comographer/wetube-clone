7861b76caa77e8c52a5d5b02402eecdf4f06d5d7
Geunmo Kim <kimgeunmo@gmail.com>
2022-01-09 19:08:35 +0100

    Setup Pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,8 @@
-export const trending = (req, res) => res.send("Trending Videos");
+export const trending = (req, res) => res.render("home");
 
-export const see = (req, res) => {
-  return res.send(`Watch Video # ${req.params.id}`);
-};
+export const see = (req, res) => res.render("watch");
 
-export const edit = (req, res) => {
-  return res.send("Edit Video");
-};
+export const edit = (req, res) => res.render("edit");
 
 export const search = (req, res) => res.send("Search Video");
 
