9176f5a34d032652f038357f4931a4d4422b9520
Geunmo Kim <kimgeunmo@gmail.com>
2022-01-10 22:45:34 +0100

    Array Database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,35 +1,39 @@
+let videos = [
+  {
+    title: "Video #1",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Video #2",
+    rating: 2,
+    comments: 21,
+    createdAt: "5 minutes ago",
+    views: 540,
+    id: 2,
+  },
+  {
+    title: "Video #3",
+    rating: 4,
+    comments: 24,
+    createdAt: "3 minutes ago",
+    views: 700,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Video #1",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Video #2",
-      rating: 2,
-      comments: 21,
-      createdAt: "5 minutes ago",
-      views: 540,
-      id: 2,
-    },
-    {
-      title: "Video #3",
-      rating: 4,
-      comments: 24,
-      createdAt: "3 minutes ago",
-      views: 700,
-      id: 3,
-    },
-  ];
   res.render("home", { pageTitle: "Home", videos });
 };
 
-export const see = (req, res) =>
-  res.render("watch", { pageTitle: "Watch", videoNumber: `${req.params.id}` });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 
 export const edit = (req, res) =>
   res.render("edit", { pageTitle: "Edit", videoNumber: `${req.params.id}` });
