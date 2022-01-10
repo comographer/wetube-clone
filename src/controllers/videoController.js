5f29ec6dcb406c4930a22a932d9ac2f63dc94ac5
Geunmo Kim <kimgeunmo@gmail.com>
2022-01-10 14:30:54 +0100

    Pug mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,30 @@
 export const trending = (req, res) => {
-  const videos = [];
+  const videos = [
+    {
+      title: "Video #1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Video #2",
+      rating: 2,
+      comments: 21,
+      createdAt: "5 minutes ago",
+      views: 540,
+      id: 2,
+    },
+    {
+      title: "Video #3",
+      rating: 4,
+      comments: 24,
+      createdAt: "3 minutes ago",
+      views: 700,
+      id: 3,
+    },
+  ];
   res.render("home", { pageTitle: "Home", videos });
 };
 
