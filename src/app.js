a97115451b745e70945a39b7af9b77367723d8a7
Geunmo Kim <kimgeunmo@gmail.com>
2022-01-11 20:46:07 +0100

    Practice GET & POST

--- src/app.js
+++ src/app.js
@@ -7,10 +7,13 @@ import videoRouter from "./routers/videoRouter";
 const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
