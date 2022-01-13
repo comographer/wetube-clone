368d6f07db8f37eda4807dc179ddaf922b4e43ee
Geunmo Kim <kimgeunmo@gmail.com>
2022-01-13 22:10:50 +0100

    Study Callback function

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,9 @@
 import Video from "../models/Videos";
 
 export const home = (req, res) => {
-  Video.find({}, (error, videos) => {});
-  res.render("home", { pageTitle: "Home" });
+  Video.find({}, (error, videos) => {
+    res.render("home", { pageTitle: "Home", videos });
+  });
 };
 
 export const watch = (req, res) => {
