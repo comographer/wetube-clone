d0d938cc0d895c9bf6ec3ec797a3bd39d73cd961
Geunmo Kim <kimgeunmo@gmail.com>
2022-01-19 21:59:09 +0100

    Add express-session

--- src/app.js
+++ src/app.js
@@ -1,18 +1,34 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
 
+// View engine Pug to render client
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
+// Middleware Morgan : display method and status
 app.use(logger);
+// Middleware to allow access to req.body
 app.use(express.urlencoded({ extended: true }));
+// Middleware to send cookie to browser
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
+app.use(localsMiddleware);
+
+// Connection to routers
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
