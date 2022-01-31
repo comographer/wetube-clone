8d912291401ea7142897ab7593c8ee550428ae02
Geunmo Kim <kimgeunmo@gmail.com>
2022-01-31 20:27:59 +0100

    Fullscreen

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,12 +1,18 @@
 const video = document.querySelector("video");
+const videoContainer = document.getElementById("videoContainer");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+const fullScreen = document.getElementById("fullScreen");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
+
+let videoPlayStatus = false;
+let setVideoPlayStatus = false;
 
 const handlePlay = () => {
   if (video.paused) {
@@ -41,14 +47,44 @@ const handleVolume = (e) => {
 };
 
 const formatTime = (seconds) =>
-  new Date(seconds * 1000).toISOString().substr(14, 5);
+  new Date(seconds * 1000).toISOString().substring(14, 19);
 
 const handleMetadata = () => {
   totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = () => {
   currentTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (e) => {
+  const {
+    target: { value },
+  } = e;
+  if (!setVideoPlayStatus) {
+    videoPlayStatus = video.paused ? false : true;
+    setVideoPlayStatus = true;
+  }
+  video.pause();
+  video.currentTime = value;
+};
+
+const handleTimelineSet = () => {
+  videoPlayStatus ? video.play() : video.pause();
+  setVideoPlayStatus = false;
+};
+
+const handleFullscreen = () => {
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    document.exitFullscreen();
+    fullScreen.className = "fas fa-expand";
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreen.className = "fas fa-compress";
+  }
 };
 
 playBtn.addEventListener("click", handlePlay);
@@ -56,3 +92,6 @@ muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolume);
 video.addEventListener("loadedmetadata", handleMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
+timeline.addEventListener("change", handleTimelineSet);
+fullScreen.addEventListener("click", handleFullscreen);
