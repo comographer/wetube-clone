62732d95f3ce6ed1ff231b220f89dc86caebdad8
Geunmo Kim <kimgeunmo@gmail.com>
2022-01-31 19:05:14 +0100

    Video Time and Formatting

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,58 @@
-console.log("Ready");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const handlePlay = () => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.className = video.paused ? "fas fa-play" : "fas fa-pause";
+};
+
+const handleMute = () => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.className = video.muted ? "fas fa-volume-mute" : "fas fa-volume-up";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolume = (e) => {
+  const {
+    target: { value },
+  } = e;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.className = "fas fa-volume-mute";
+  }
+  muteBtn.className = value === "0" ? "fas fa-volume-mute" : "fas fa-volume-up";
+  volumeValue = value;
+  video.volume = value;
+};
+
+const formatTime = (seconds) =>
+  new Date(seconds * 1000).toISOString().substr(14, 5);
+
+const handleMetadata = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+};
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+};
+
+playBtn.addEventListener("click", handlePlay);
+muteBtn.addEventListener("click", handleMute);
+volumeRange.addEventListener("input", handleVolume);
+video.addEventListener("loadedmetadata", handleMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
